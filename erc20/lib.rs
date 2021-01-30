b2dd062ec49ff5055980c9708e19c4e49cd5177f
李峻 <lijun@192.168.2.3>
2021-01-31 01:02:29 +0800

    第5课作业

---
+++ erc20/lib.rs
@@ -0,0 +1,145 @@
+#![cfg_attr(not(feature = "std"), no_std)]
+
+use ink_lang as ink;
+
+#[ink::contract]
+mod erc20 {
+    #[cfg(not(feature = "ink-as-dependency"))]
+    use ink_storage::collections::HashMap as StorageHashMap;
+
+    #[ink(storage)]
+    pub struct Erc20 {
+        total_supply: Balance,
+        balances: StorageHashMap<AccountId, Balance>,
+        allowances: StorageHashMap<(AccountId, AccountId), Balance>,
+ 
+    }
+    #[ink(event)]
+    pub struct Transfer {
+        #[ink(topic)]
+        from: Option<AccountId>,
+        #[ink(topic)]
+        to: Option<AccountId>,
+        #[ink(topic)]
+        value: Balance,
+    }
+
+
+    #[derive(Debug, PartialEq, Eq, scale::Encode)]
+    #[cfg_attr(feature = "std", derive(scale_info::TypeInfo))]
+    pub enum Error {
+        InsufficientBalance,
+        InsufficientAllowance,
+    }
+        
+    pub type Result<T> = core::result::Result<T, Error>;
+
+    impl Erc20 {
+        /// Constructor that initializes the `bool` value to the given `init_value`.
+        #[ink(constructor)]
+        pub fn new(total_supply: Balance) -> Self {
+            let caller = Self::env().caller();
+            let mut balances = StorageHashMap::new();
+            balances.insert( caller, total_supply );
+            let instance = Self {
+                total_supply: total_supply,
+                balances: balances,
+                allowances: StorageHashMap::new(),
+            };
+            Self::env().emit_event( Transfer {
+                from: None,
+                to: Some(caller),
+                value: total_supply,
+            });
+            instance
+        }
+
+        #[ink(message)]
+        pub fn total_supply(&self) -> Balance {
+            self.total_supply
+        }
+        
+
+        #[ink(message)] 
+        pub fn balance_of(&self, owner: AccountId) -> Balance {
+            self.balances.get(&owner).copied().unwrap_or(0)
+        }
+
+        // ---------作业  BEGIN  ------------
+        #[ink(message)]
+        pub fn allowance(&self, owner: AccountId, spender: AccountId) -> Balance {
+            self.allowances.get(&(owner, spender)).copied().unwrap_or(0)
+        }
+
+        #[ink(message)]
+        pub fn transfer_from(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let caller = self.env().caller();
+            let allowance = self.allowance(from, caller);
+            if allowance < value {
+                return Err(Error::InsufficientAllowance)
+            }
+            self.transfer_help(from, to, value)?;
+            self.allowances.insert((from, caller), allowance - value);
+            Ok(())
+        }
+    // --------------作业   END------------------
+
+        #[ink(message)]
+        pub fn transfer(&mut self, to: AccountId, value: Balance) -> Result<()> {
+            let from = self.env().caller();
+            self.transfer_help(from, to, value)
+        }
+
+        fn transfer_help(
+            &mut self,
+            from: AccountId,
+            to: AccountId,
+            value: Balance,
+        ) -> Result<()> {
+            let from_balance = self.balance_of(from);
+            if from_balance < value {
+                return Err(Error::InsufficientBalance)
+            }
+            self.balances.insert(from, from_balance - value);
+            let to_balance = self.balance_of(to);
+            self.balances.insert(to, to_balance + value);
+            self.env().emit_event(Transfer {
+                from: Some(from),
+                to: Some(to),
+                value,
+            });
+            Ok(())
+        }
+
+    }
+
+    /// Unit tests in Rust are normally defined within such a `#[cfg(test)]`
+    /// module and test functions are marked with a `#[test]` attribute.
+    /// The below code is technically just normal Rust code.
+    #[cfg(test)]
+    mod tests {
+        /// Imports all the definitions from the outer scope so we can use them here.
+        use super::*;
+
+        /// We test if the default constructor does its job.
+        #[test]
+        fn default_works() {
+            let erc20 = Erc20::default();
+            assert_eq!(erc20.get(), false);
+        }
+
+        /// We test a simple use case of our contract.
+        #[test]
+        fn it_works() {
+            let mut erc20 = Erc20::new(false);
+            assert_eq!(erc20.get(), false);
+            erc20.flip();
+            assert_eq!(erc20.get(), true);
+        }
+    }
+}
